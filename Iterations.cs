8393c0c55df179ff81981e46c7b0f685457425a5
Michelle Hendrix <michelle49@mail.usf.edu>
2020-02-18 22:38:36 -0500

    initial commit

--- Iterations.cs
+++ Iterations.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using DotnetIteration.Models;
 
-namespace DotnetIteration
-{
+namespace DotnetIteration {
 
-  public static class Iteration
-  {
+  public static class Iteration {
     /*
        Welcome to Interation
        Down below you will find instructions for code to write.
@@ -23,109 +21,111 @@ namespace DotnetIteration
       * strings as an argument and returns a new list with all
       * the words forced to uppercase
       *
-    
+     
    */
-    public static IEnumerable<string> Yelling(List<string> words)
-    {
-      throw new System.NotImplementedException();
+    public static IEnumerable<string> Yelling (List<string> words) {
+      return words.Select (word => word.ToUpper ());
     }
 
     /* 
-      * 2) Define a function named `doubleTrouble` that takes an list of
-      * numbers as an argument and returns a new list with all
-      * the numbers multiplied by 2
-      */
+     * 2) Define a function named `doubleTrouble` that takes an list of
+     * numbers as an argument and returns a new list with all
+     * the numbers multiplied by 2
+     */
 
-    public static IEnumerable<int> Double(List<int> numbers)
-    {
-      throw new System.NotImplementedException();
+    public static IEnumerable<int> Double (List<int> numbers) {
+      var doubleTrouble = numbers.Select (number => number * 2);
+      return doubleTrouble;
     }
     /*
-      * 3) Define a function stringyIndexes() that takes an list of
-      * strings as an argument and returns a new list with each string
-      * suffixed with " is at index X" where X is the index of the element
-      */
-    public static IEnumerable<string> StringyIndexes(List<string> data)
-    {
-      throw new System.NotImplementedException();
+     * 3) Define a function stringyIndexes() that takes an list of
+     * strings as an argument and returns a new list with each string
+     * suffixed with " is at index X" where X is the index of the element
+     */
+    public static IEnumerable<string> StringyIndexes (List<string> data) {
+      var stringyIndexes = data.Select (item => { return item + " is at index " + data.IndexOf (item); });
+      return stringyIndexes;
     }
     /*
-       * 4) Define a function onlyTheEvenSurvive that accepts an list of
-       * numbers and returns only the elements that are even
-       */
+     * 4) Define a function onlyTheEvenSurvive that accepts an list of
+     * numbers and returns only the elements that are even
+     */
 
-    public static IEnumerable<int> OnlyTheEvenSurvive(List<int> data)
-    {
-      throw new System.NotImplementedException();
+    public static IEnumerable<int> OnlyTheEvenSurvive (List<int> data) {
+      var onlyTheEvenSurvive = data.Where (numbers => numbers % 2 == 0);
+      return onlyTheEvenSurvive;
     }
     /*
-        * 5) Define a function onlyTheEvenIndexedSurvive that accepts an list of
-        * numbers and returns only the elements at indexes that are even
-        */
-    public static IEnumerable<int> OnlyTheEvenIndexedSurvive(List<int> data)
-    {
-      throw new System.NotImplementedException();
+     * 5) Define a function onlyTheEvenIndexedSurvive that accepts an list of
+     * numbers and returns only the elements at indexes that are even
+     */
+    public static IEnumerable<int> OnlyTheEvenIndexedSurvive (List<int> data) {
+      var OnlyTheEvenIndexedSurvive = data.Where (number => data.IndexOf (number) % 2 == 0);
+      return OnlyTheEvenIndexedSurvive;
     }
     /*
-      * 6)  Define a function bestMoviesOfTheYear that accepts an list of
-      * movie objects AND a year and returns the names of movies that are
-      * from that year AND have a score more than 90
-      *
-      * A movie object looks like this:
-      *
-      * {
-      *   name: "Get Out",
-      *   year: "2017",
-      *   score: 99
-      * }
-      */
-    public static IEnumerable<string> BestMovieOfTheYear(List<Movie> data, int year)
-    {
-      throw new System.NotImplementedException();
-    }
-
-    /*
-      * 7) Define a function everyoneIsOdd that accepts an list of
-      * numbers and returns true if every element of the list is
-      * odd.
-      */
-
-    public static bool EveryoneIsOdd(List<int> data)
-    {
-      throw new System.NotImplementedException();
-    }
-    /*
-      * 8) Define a function findTheNeedle that accepts an list of
-      * strings and returns the one string that contains the word
-      * `needle` 
-      */
-    public static string FindTheNeedle(List<string> data)
-    {
-      throw new System.NotImplementedException();
-    }
-
-
-    /*
-        * 9) Define a function findTheNeedleIndex that accepts an list of
-        * strings and returns the index of the string that contains
-        *  the word `needle` inside
-        */
-
-    public static int FindTheNeedleIndex(List<string> data)
-    {
-      throw new System.NotImplementedException();
+     * 6)  Define a function bestMoviesOfTheYear that accepts an list of
+     * movie objects AND a year and returns the names of movies that are
+     * from that year AND have a score more than 90
+     *
+     * A movie object looks like this:
+     *
+     * {
+     *   name: "Get Out",
+     *   year: "2017",
+     *   score: 99
+     * }
+     */
+    public static IEnumerable<string> BestMovieOfTheYear (List<Movie> data, int year) {
+      var bestMovieOfTheYear = data.Where (movie => movie.Year == year && movie.Score > 90)
+        .Select (movie => movie.Name);
+      return bestMovieOfTheYear;
     }
 
     /*
-      *` 10)  Define a function someoneToLove that accepts an list of
-      * strings and returns true if at least one string is exactly
-      * four characters long
-      */
+     * 7) Define a function everyoneIsOdd that accepts an list of
+     * numbers and returns true if every element of the list is
+     * odd.
+     */
 
+    public static bool EveryoneIsOdd (List<int> data) {
+      var everyoneIsOdd = data.Where (number => number % 2 == 1);
+      return everyoneIsOdd.Count () == data.Count;
+    }
+    /*
+     * 8) Define a function findTheNeedle that accepts an list of
+     * strings and returns the one string that contains the word
+     * `needle` 
+     */
+    public static string FindTheNeedle (List<string> data) {
+      var findTheNeedle = data.Where (word => word.Contains ("needle"));
+      return findTheNeedle.First ();
 
-    public static bool SomeoneToLove(List<string> data)
-    {
-      throw new System.NotImplementedException();
+    }
+
+    /*
+     * 9) Define a function findTheNeedleIndex that accepts an list of
+     * strings and returns the index of the string that contains
+     *  the word `needle` inside
+     */
+
+    public static int FindTheNeedleIndex (List<string> data) {
+      var findTheNeedle = data.Where (word => word.Contains ("needle"));
+      var needle = findTheNeedle.First ();
+      return data.IndexOf (needle);
+
+    }
+
+    /*
+     *` 10)  Define a function someoneToLove that accepts a list of
+     * strings and returns true if at least one string is exactly
+     * four characters long
+     */
+
+    public static bool SomeoneToLove (List<string> data) {
+      var someoneToLove = data.Where (word => word.Length == 4);
+      return someoneToLove.Any ();
+
     }
   }
 
